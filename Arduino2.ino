5238b23b08eccecd15da58d527f91c3cd0737d06
MarvinStrasser <97129670+MarvinStrasser@users.noreply.github.com>
2023-06-07 20:25:02 +0200

    Create Arduino2.ino

---
+++ Arduino2.ino
@@ -0,0 +1,30 @@
+#include<SPI.h>                   // spi library for connecting nrf
+#include<RF24.h>                  // nrf library
+
+RF24 radio(9, 10) ;  // ce, csn pins    
+
+void setup(void) {
+  while (!Serial) ;
+  Serial.begin(9600) ;     // start serial monitor baud rate
+  radio.begin();        // start radio at ce csn pin 9 and 10
+  radio.setPALevel(RF24_PA_MAX) ;   // set power level
+  radio.setChannel(0x76) ;            // set chanel at 76
+  const uint64_t pipe = 0xE0E0F1F1E0LL ;    // pipe address same as sender i.e. raspberry pi
+  radio.openReadingPipe(1, pipe) ;        // start reading pipe 
+  radio.enableDynamicPayloads() ;
+  radio.powerUp() ;          
+  delay(2000);
+  radio.startListening() ; 
+}
+
+void loop(void) {
+//  radio.startListening() ;        // start listening forever
+  char receivedMessage[32] = {0} ;   // set incmng message for 32 bytes
+  if (radio.available()) {       // check if message is coming
+    radio.read(receivedMessage, sizeof(receivedMessage));    // read the message and save
+    Serial.println(receivedMessage) ;    // print message on serial monitor 
+//    radio.stopListening() ;   // stop listening radio
+    //delay(1000);    // delay of 1 second 
+  }
+  delay(10);
+}
